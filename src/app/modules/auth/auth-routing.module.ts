5687c20bea695f54f7c0ea55b723d54b86100ab8
Safikul Islam <islam.safikul@hotmail.com>
2022-12-19 10:39:05 +0530

    some module deleted

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -9,9 +9,7 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', loadChildren: () => import("./home/home.module").then(m => m.HomeModule) },
-      { path: 'notes', loadChildren: () => import('./notes/notes.module').then(m => m.NotesModule) },
       { path: 'connection', loadChildren: () => import('./connection/connection.module').then(m => m.ConnectionModule) },
-      { path: 'storage', loadChildren: () => import('./storage/storage.module').then(m => m.StorageModule) },
       { path: 'app-settings', loadChildren: () => import("./settings/app-settings/app-settings.module").then(m => m.AppSettingsModule) },
       { path: '**', redirectTo: 'home' }
     ]
